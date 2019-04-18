d2d04c71ebdf54fe3bc34c87458a27d0c23f09c7
jueduihuangfu <787936777@qq.com>
2019-04-18 14:12:50 +0800

    去掉没登陆的时候底下的登陆广告条

--- 去掉CSDN广告.js
+++ 去掉CSDN广告.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         CSDN广告去除
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
-// @author       You
+// @version      0.3
+// @description  CSDN页面太乱太杂，广告也很多，这个脚本去掉这些个东西，让页面看起来清爽一点
+// @author       Huangfu
 // @include      https://blog.csdn.net/*/article/details/*
 // @grant        none
 // ==/UserScript==
@@ -12,7 +12,11 @@
 (function() {
     'use strict';
     function remove(add){
-        add.parentNode.removeChild(add);
+        try{
+            add.parentNode.removeChild(add);
+        }catch(e){
+            console.log(e);
+        }
     }
     //移除左栏
     var tag = document.getElementsByTagName("aside");
@@ -29,7 +33,6 @@
     //去掉工具栏
     var bottonbar = document.getElementsByClassName("tool-box");
     remove(bottonbar[0]);
-
     var floatbar = document.getElementsByClassName("meau-gotop-box");
     remove(floatbar[0]);
     //去掉底下能去的广告
@@ -37,6 +40,9 @@
     while (adds.length!=0){
         remove(adds[0]);
     }
+    //没登陆的时候底下有登陆广告，去掉
+    var btn_bar = document.getElementsByClassName("pulllog-box");
+    remove(btn_bar[0]);
     //展开阅读更多
     var btn = document.getElementById("btn-readmore");
     btn.click();
